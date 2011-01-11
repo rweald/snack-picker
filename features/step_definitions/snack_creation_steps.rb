5d10b41e2ae7ab90f59428b83ed076d06b474cb9
Ryan Weald <ryan@weald.com>
2011-01-11 13:20:43 -0800

    Fix deprecated form call in snack index template. Also remove the old step definition for snack_creation_steps 'Given'

--- features/step_definitions/snack_creation_steps.rb
+++ features/step_definitions/snack_creation_steps.rb
@@ -1,6 +1,5 @@
 Given /^a list containing snacks$/ do
-  visit(url_for(:controller => "snacks", :action => "index"))
-  page.should have_selector("table tbody tr")
+  pending # express the regexp above with the code you wish you had
 end
 
 When /^I create a new snack using the snack form$/ do
