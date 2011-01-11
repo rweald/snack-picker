152e8134df86aef83c77464523b68ccf46db96e5
Ryan Weald <ryan@weald.com>
2011-01-10 22:26:10 -0800

    refactor snack_spec Move before constructor to top level describe

--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -1,14 +1,23 @@
 require 'spec_helper'
 
 describe Snack do
+  before(:each) do
+    @my_snack = Factory(:snack)
+    @my_snack.save
+  end
   context "validation" do
-    before(:each) do
-      Factory(:snack).save
-    end
     it { should validate_presence_of :name }
     it { should validate_uniqueness_of :name}
     it { should validate_presence_of :price}
     it { should validate_presence_of :category}
     it { should validate_presence_of :quantity}    
+  end
+  
+  describe "#update_vote_count" do
+    context "valid id parameter" do
+      it "should return true" do
+        Snack.update_vote_count(@my_snack.id).should be_true
+      end
+    end
   end
 end
