98e412145462567c7a4ccff070648bf1eae2ef57
Ryan Weald <ryan@weald.com>
2011-01-10 22:46:11 -0800

    refactor update_vote_count method

--- app/models/snack.rb
+++ app/models/snack.rb
@@ -4,4 +4,16 @@ class Snack < ActiveRecord::Base
   validates_presence_of :price
   validates_presence_of :category
   validates_presence_of :quantity
+  
+  def self.update_vote_count(args)
+    begin
+      curr_snack = Snack.find(args[:id])
+      curr_snack.votes += 1
+      return curr_snack.save
+    rescue Exception => e
+      return false
+    end
+    
+    false
+  end
 end
--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -16,8 +16,22 @@ describe Snack do
   describe "#update_vote_count" do
     context "valid id parameter" do
       it "should return true" do
-        Snack.update_vote_count(@my_snack.id).should be_true
+        Snack.update_vote_count(:id => @my_snack.id).should be_true
       end
+      
+      it "should increase the number of votes by 1" do
+        old_number = @my_snack.votes
+        Snack.update_vote_count(:id => @my_snack.id)
+        Snack.find(@my_snack.id).votes.should == (old_number + 1)
+      end
+    end
+    
+    context "invalid id parameter" do
+      it "should return false" do
+        Snack.update_vote_count(:id => nil).should_not be_true
+        Snack.any_instance.expects(:save).returns(false)
+        Snack.update_vote_count(:id => @my_snack.id).should_not be_true
+      end        
     end
   end
 end
